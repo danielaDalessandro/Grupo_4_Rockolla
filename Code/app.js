6346820415a09d124e8c63c959253ca835dbca44
Facundo Erbin <facuerbin@gmail.com>
2020-07-14 14:29:04 -0300

    Creo servidor express

--- Code/app.js
+++ Code/app.js
@@ -1,1 +1,27 @@
-console.log ("Creo carpeta de code mas archivo app.js")
+const express = require ("express");
+
+const app = express();
+const port = 3000;
+
+//Static files to be used
+app.use(express.static('public'));
+
+//Landing page
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+});
+
+/*
+app.get("/route", (req, res) => {
+    res.sendFile(__dirname + "/views/route.html");
+});
+*/
+
+//Any other page
+app.get("/*", (req,res) =>{
+	res.sendFile(__dirname + "/views/404.html")
+});
+
+app.listen(port, () => {
+    console.log("Servidor escuchando en el puerto: ", port);
+});
