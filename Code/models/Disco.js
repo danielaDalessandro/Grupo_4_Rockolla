aa0c8012fb9fdfbf24cdbd2938a9e43685c4bb6d
Facundo Erbin <facuerbin@gmail.com>
2020-07-26 22:10:33 -0300

    agrego carousel y contenido al home

---
+++ Code/models/Disco.js
@@ -0,0 +1,22 @@
+/**
+ * Discos.js
+ */
+
+module.exports = {
+    attributes: {
+      titulo: { type: 'string' },
+  
+      artista: { type: 'string' },
+  
+      sello: { type: 'string' },
+  
+      genero: {type: 'Genero'},
+    
+      fechaPublicacion: { type: 'Date' },
+    
+      tapa: {type: 'Tapa'},
+
+      canciones: {type: 'Canciones'}
+    },
+  };
+  
