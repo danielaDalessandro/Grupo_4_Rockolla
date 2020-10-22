1b1b47a9c86a3ac2f986a60a34e3a84d7ba27f58
facuerbin <facuerbin@gmail.com>
2020-10-22 18:22:09 -0300

    Agrego vista de contacto

--- Code/controllers/indexController.js
+++ Code/controllers/indexController.js
@@ -1,27 +1,39 @@
 const db = require("../database/models");
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 module.exports = {
-    index: (req, res) => {
-        db.product.findAll({
-            attributes: [
-                'id',
-                'cover',
-                'title',
-                'price',
-                [Sequelize.col('artist.name'), 'artist']
-            ],
-            include : [
-                "artist"
-            ],
-            where: {
-                highlight: 1
-            },
-            raw: true
-        })
-        .then( function (destacados) {
-            res.render('index', {destacados});
-        })
-        .catch( e => {console.log("indes error; ",e)})
-        }
-}
+  index: async (req, res) => {
+    let destacados = await db.product
+      .findAll({
+        attributes: [
+          "id",
+          "cover",
+          "title",
+          "price",
+          [Sequelize.col("artist.name"), "artist"],
+        ],
+        include: ["artist"],
+        where: {
+          highlight: 1,
+        },
+        raw: true,
+      })
+      .catch((e) => {
+        console.log("INDEX; ", e);
+      });
+
+    let recientes = await db.sequelize
+      .query(
+        "SELECT * FROM rockolla_db.products where deleted_at is null order by updated_at desc limit 15"
+      )
+      .catch((e) => {
+        console.log("INDEX; ", e);
+      });
+    console.log(recientes);
+    res.render("index", { destacados, recientes: recientes[0] });
+  },
+
+  contact: (req, res) => {
+    res.render("contact");
+  }
+};
--- Code/routes/index.js
+++ Code/routes/index.js
@@ -5,4 +5,7 @@ const controller = require('../controllers/indexController');
 
 router.get('/', controller.index);
 
+router.get('/contact', controller.contact);
+
+
 module.exports = router;
